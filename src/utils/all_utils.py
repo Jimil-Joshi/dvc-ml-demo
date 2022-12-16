34df5e0185ab2a5bf6a27b84599f33a6dd2202ef
Joshi Jimil <jjjimiljoshi14@gmail.com>
2022-12-16 16:17:01 +0530

    Project Structure added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 
-def read_yaml(path_to_yaml : str) -> dict:
+def read_config(path_to_yaml : str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
     return content
